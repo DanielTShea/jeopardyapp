675118c6c61881371389031ff464a68474115c17
dshea18 <dshea18@LAPTOP-2MS7R1O2.localdomain>
2023-04-17 14:56:38 -0400

    added new components

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () =>{
+
+    return(
+        <nav className="navbar">
+            <h1>Jeopardy</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/create">New Game</a>
+            </div>
+        </nav>
+    );
+
+}
+
+export default Navbar;
