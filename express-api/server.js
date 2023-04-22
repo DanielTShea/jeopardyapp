713e525e55774ff8d96ebc72e8abc25d7be4a32d
dshea18 <dshea18@LAPTOP-2MS7R1O2.localdomain>
2023-04-22 10:29:10 -0400

    updates to API

--- express-api/server.js
+++ express-api/server.js
@@ -1,12 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 
-//Express Application
+//Express app
 const app = express();
-
 
 //Listen for requests
 const port = process.env.PORT || 4000;
-console.log(process.env.PORT);
-console.log(process.env.DATABSE_URL);
-app.listen(port, ()=> console.log(`listening on port ${port}`));
-
+console.log(process.env.PORT)
+app.listen(port, ()=>console.log(`Listening on port ${port}`));
