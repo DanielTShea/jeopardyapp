bdc8718753a344c70d46040110f60ddd1e1ec0d1
Daniel Shea <84584889+DanielTShea@users.noreply.github.com>
2023-04-23 17:39:30 -0400

    added first routes (#3)
    Co-authored-by: dshea18 <dshea18@LAPTOP-2MS7R1O2.localdomain>

--- express-api/server.js
+++ express-api/server.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const gameRoutes = require('./routes/games')
 
 //Express app
 const app = express();
 
+//Middleware
+app.use(express.json());
+
+//Log requests to console
+app.use((req, res, next) =>{
+    console.log(req.path, req.method);
+    next();
+});
+
+//Routes
+app.use('/api/games', gameRoutes);
+
 //Listen for requests
 const port = process.env.PORT || 4000;
-console.log(process.env.PORT)
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
