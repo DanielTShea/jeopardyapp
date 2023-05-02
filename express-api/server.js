c4285e0ac0544b13d6215b5b55608faa21f40834
Daniel Shea <84584889+DanielTShea@users.noreply.github.com>
2023-05-02 18:06:27 -0400

    added mongoDB (#4)
    Co-authored-by: dshea18 <dshea18@LAPTOP-2MS7R1O2.localdomain>

--- express-api/server.js
+++ express-api/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const gameRoutes = require('./routes/games')
 
 //Express app
@@ -17,6 +19,14 @@ app.use((req, res, next) =>{
 //Routes
 app.use('/api/games', gameRoutes);
 
-//Listen for requests
-const port = process.env.PORT || 4000;
-app.listen(port, ()=>console.log(`Listening on port ${port}`));
+//Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //Listen for requests
+        const port = process.env.PORT || 4000;
+        app.listen(port, ()=>console.log(`connected to DB and listening on port ${port}`));
+    })
+    .catch((error) =>{
+        console.log(error);
+    })
+
